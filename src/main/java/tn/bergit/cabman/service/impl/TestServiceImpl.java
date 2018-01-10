e2d75396dbf92ba8433fd7dd41bde4dd6d6e9cde
BERGUIGA AMINE <amine.berguiga@holydis.com>
2018-01-10 17:17:21 +0100

    Ajouter l'annotation transaction de spring

--- src/main/java/tn/bergit/cabman/service/impl/TestServiceImpl.java
+++ src/main/java/tn/bergit/cabman/service/impl/TestServiceImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import tn.bergit.cabman.dto.TestDTO;
 import tn.bergit.cabman.repository.TestRepository;
@@ -22,12 +24,14 @@ public class TestServiceImpl implements TestService {
 	TestRepository gTestRepository;
 	
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly=true)
 	public List<TestDTO> findAllByCategorie(String iCategorieName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly=true)
 	public void testFn() {
 		gTestRepository.testFn();
 	}
